120a529416ed5aacd2dceed133960c4867fb060b
general <you@example.com>
2017-05-23 09:59:36 +0300

    S2T1: Prepare base React application

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {
+	entry: './dev/main.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	// devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					loader: ['css-loader','sass-loader']
+				})
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: [
+					'file-loader?name=images/[name].[ext]',
+					'image-webpack-loader'
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "build"),
+		compress: true,
+		port: 8090,
+		stats: "errors-only",
+		open: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'CityElf',
+			minify: {
+				collapseWhitespace: true
+			},
+			template: './dev/index.html'
+		}),
+		new ExtractTextPlugin({
+			filename: 'style.css',
+			disable: false
+		})		
+	]
+};
