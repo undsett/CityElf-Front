bf05291b14438d0d2f98ef6a270f66109b69af5f
general <you@example.com>
2017-05-24 00:33:09 +0300

    S2T2: Data transfer service from/to server
    S2T2: Data transfer service from/to server

--- dev/components/App.js
+++ dev/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import {userResources} from '../resources/userResources.js';
+
+
+
 export default class App extends React.Component {
 	
+	constructor(){
+		super();
+	}
+
+	componentWillMount() {
+		userResources.getAllUsers();
+	}
 
 	render() {		
 		return <div>Hello from React Component</div>
